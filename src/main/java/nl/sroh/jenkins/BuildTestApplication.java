a241c09c488982688e5dde6233dafb3a1408ddd2
srohde <sven.rohde@fdmediagroep.nl>
2019-07-15 23:54:39 +0200

    Initial commit of example code

---
+++ src/main/java/nl/sroh/jenkins/BuildTestApplication.java
@@ -0,0 +1,13 @@
+package nl.sroh.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildTestApplication.class, args);
+	}
+
+}
